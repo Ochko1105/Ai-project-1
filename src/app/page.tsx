ecd8fce22426f5b5b264e440d0bf70ccd6f31cab
Naransolongo <tsetsegulziiocherdene@gmail.com>
2025-12-25 19:47:41 +0800

    ochko

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,61 +1,11 @@
-"use client";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import ImageToText from "@/components/Home/imageToText";
 
 import TextToImage from "@/components/Home/textToImage";
 import TextToText from "@/components/Home/TextToText";
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "@/components/ui/drawer";
-import { Button } from "@/components/ui/button";
-import { useState } from "react";
-import { Input } from "@/components/ui/input";
 
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuPortal,
-  DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-
-export function Project() {
-  const [text, setText] = useState("");
-  const [resdata, setResdata] = useState("");
-  // const getText = async () => {
-  //   const result = await fetch("/api/chatbot");
-  //   const responseData = await result.json();
-  //   const { text } = responseData;
-  //   setText(text);
-  // };
-  const HandleOnPost = async () => {
-    const response = await fetch("/api/chatbot", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ text }),
-    });
-    console.log({ response });
-    const data = await response.json();
-    console.log({ data });
-    setResdata(data.text);
-  };
+function Page() {
   return (
     <div className="flex w-[580px] h-fit max-w-sm flex-col gap-6 ml-100 mt-50">
       <Tabs defaultValue="account">
@@ -87,4 +37,4 @@ export function Project() {
   );
 }
 
-export default Project;
+export default Page;
